0a2db37e60548a5e7f64b6eb5c520aca4f91076b
derricknoutais <derricknoutais@gmail.com>
2023-12-19 15:00:50 +0100

    Updating Handles Definition

--- app/Jobs/InsertPulledProductsToDatabase.php
+++ app/Jobs/InsertPulledProductsToDatabase.php
@@ -63,7 +63,7 @@ class InsertPulledProductsToDatabase implements ShouldQueue
         // Inserer Final Produits dans Database
         // DB::table('products')->upsert($final_prods->toArray(), ['id'], ['id', 'name', 'variant_parent_id', 'variant_name', 'variant_option_one_value', 'variant_option_two_value', 'variant_option_three_value', 'handle_name', 'sku', 'price_including_tax', 'price_excluding_tax', 'active', 'has_inventory', 'is_composite', 'description', 'created_at', 'updated_at', 'deleted_at', 'source', 'supply_price', 'version', 'type', 'is_active']);
         Product::all()->map->delete();
-        foreach (array_chunk($final_prods->toArray(), 100) as $data) {
+        foreach (array_chunk($final_prods->toArray(), 1000) as $data) {
             Log::info(sizeof($data));
             DB::table('products')->insert($data);
             // DB::table('products')->upsert($data, ['id'], ['id', 'name', 'variant_parent_id', 'variant_name', 'variant_option_one_value', 'variant_option_two_value', 'variant_option_three_value', 'handle_name', 'sku', 'price_including_tax', 'price_excluding_tax', 'active', 'has_inventory', 'is_composite', 'description', 'created_at', 'updated_at', 'deleted_at', 'source', 'supply_price', 'version', 'type', 'is_active']);

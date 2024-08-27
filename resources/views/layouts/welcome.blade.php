7c073a751282accc15ed4980de1a16732fa7a206
derricknoutais <derricknoutais@gmail.com>
2024-08-27 09:27:07 +0100

    Adding Translation Link

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="UTF-8">
@@ -7,9 +8,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css"
+        integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
 
 </head>
+
 <body>
     {{-- Navigation --}}
     <div id="app" class="tw-h-screen tw-flex tw-flex-col">
@@ -26,7 +29,8 @@
                     <a class="tw-mx-3" href="/commande">Commandes</a>
                     <a class="tw-mx-3" href="/product">Produits</a>
                     <a class="tw-mx-3" href="/fournisseur">Fournisseur</a>
-                    <a class="tw-mx-3" href="/inventory-count">Inventory Count</a>
+                    <a class="tw-mx-3" href="/translation">Traductions</a>
+                    {{-- <a class="tw-mx-3" href="/inventory-count">Inventory Count</a> --}}
                 </nav>
             @else
                 <nav>
@@ -43,9 +47,11 @@
         <footer class="tw-container-fluid tw-bg-gray-900 tw-text-white tw-p-5 tw-bottom-0">
             <p>Services Tous Azimuts</p>
             <div class="tw-flex tw-items-center">
-                <img src="https://cdn.icon-icons.com/icons2/159/PNG/256/facebook_22567.png" class="tw-w-5 tw-m-3"/>
-                <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/300px-Twitter_Bird.svg.png" class="tw-w-5 tw-m-3"/>
-                <img src="https://cdn.icon-icons.com/icons2/914/PNG/512/Instagram_Color_icon-icons.com_71811.png" class="tw-w-6 tw-m-3" />
+                <img src="https://cdn.icon-icons.com/icons2/159/PNG/256/facebook_22567.png" class="tw-w-5 tw-m-3" />
+                <img src="https://upload.wikimedia.org/wikipedia/fr/thumb/c/c8/Twitter_Bird.svg/300px-Twitter_Bird.svg.png"
+                    class="tw-w-5 tw-m-3" />
+                <img src="https://cdn.icon-icons.com/icons2/914/PNG/512/Instagram_Color_icon-icons.com_71811.png"
+                    class="tw-w-6 tw-m-3" />
             </div>
         </footer>
 
@@ -54,4 +60,5 @@
 
     <script src="/js/app.js"></script>
 </body>
+
 </html>
